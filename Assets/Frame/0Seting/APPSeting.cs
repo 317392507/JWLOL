08e1e5a7f15de0c8f5efd1a5fc7ddefd86c0c8bd
317392507 <33458839+317392507@users.noreply.github.com>
2022-07-27 06:19:56 +0800

    初始化工程
    第一次提交  仅包含kcp通讯

---
+++ Assets/Frame/0Seting/APPSeting.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class APPSeting
+{
+    public static string IP = "127.0.0.1";
+    public static int port = 8899;
+}
---
+++ Server/JW_MOBA_Server/Frame/Setting/APPSeting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public static class APPSeting
+{
+    public static string IP = "127.0.0.1";
+    public static int port = 8899;
+
+    /// <summary>
+    /// 重发最大次数
+    /// </summary>
+    public static uint resendLimit = 10;
+}
+
